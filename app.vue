42fe97984e5b0307504cd7165e69587644050c6f
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-04-01 16:30:41 +0900

    leftside bar added

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{ 'dark': darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6
+        lg:max-w-7xl lg:gap-5 lg:px-8">
+          <aside class="sm:block xs-col-span-1
+          xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </aside>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  const darkMode = ref(true);
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <div class="p-2 my-2 rounded-full hover:bg-blue-50 
+    dark:hover:bg-white/20 w-min" :class="transition">
+      <nuxt-link to="/">
+        <div class="w-8 h-8 flex items-center justify-center">
+          <img src="~/assets/logo.svg" alt="logo">
+        </div>
+      </nuxt-link>
+    </div>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <HomeIcon />
+      </template>
+      <template v-slot:name>
+        Home
+      </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <HashtagIcon />
+      </template>
+      <template v-slot:name>
+        Explore
+      </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <InboxIcon />
+      </template>
+      <template v-slot:name>
+        Messages
+      </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <BookmarkIcon />
+      </template>
+      <template v-slot:name>
+        Bookmarks
+      </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <DocumentTextIcon />
+      </template>
+      <template v-slot:name>
+        Lists
+      </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <BellIcon />
+      </template>
+      <template v-slot:name>
+        Notifications
+      </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <UserIcon />
+      </template>
+      <template v-slot:name>
+        User
+      </template>
+    </SidebarLeftTab>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <EllipsisHorizontalCircleIcon />
+      </template>
+      <template v-slot:name>
+        More
+      </template>
+    </SidebarLeftTab>
+  </div>
+</template>
+
+<script setup>
+  import { 
+    HomeIcon, HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, 
+    DocumentTextIcon, UserIcon, EllipsisHorizontalCircleIcon 
+  } from "@heroicons/vue/24/outline";
+  const { defaultTransition } = useTailwindConfig();
+
+</script>
